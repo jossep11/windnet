afe3809717051393693cc7c6da514ad789e612bd
JossepP <jossepparedes@gmail.com>
2022-07-28 15:24:12 -0400

    contact form

---
+++ app.js
@@ -0,0 +1,31 @@
+const buttonEmail = document.querySelector("#button-email_opacity");
+const form = document.querySelector("#form");
+let contador = 1;
+window.onload = () => {
+  if (document.querySelector(`#carrucel-1`)) {
+    setInterval(() => {
+      if (contador > 3) {
+        contador = 1;
+      }
+      document.querySelector(`#carrucel-${contador}`).checked = true;
+      contador++;
+    }, 5000);
+  }
+};
+if (form) {
+  form.addEventListener("submit", handleSubmit);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = new FormData(this);
+  console.log(form.get("name"));
+  buttonEmail.setAttribute(
+    "href",
+    `mailto:jossepparedes@gmail.com?subject=${form.get("name")} ${form.get(
+      "last-name"
+    )}, ${form.get("email")} &body=Número de telefono:${form.get(
+      "phone-number"
+    )}%0D%0A${form.get("message")}`
+  );
+  buttonEmail.click();
+}
